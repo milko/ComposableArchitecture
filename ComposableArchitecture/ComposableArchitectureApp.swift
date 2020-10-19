92d09cb62edc7664a8ee7a29c6fdb82d9cec33bb
Milko Škofič <skofic@gmail.com>
2020-10-19 13:38:53 +0200

    Initial Commit

---
+++ ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by Milko Škofič on 19/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
