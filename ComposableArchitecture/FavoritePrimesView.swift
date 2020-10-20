acf63128119e219eed9346ea85e510d06c66b09b
Milko Škofič <skofic@gmail.com>
2020-10-20 20:06:10 +0200

    Composable State Management: State Pullbacks. Episode #69.

---
+++ ComposableArchitecture/FavoritePrimesView.swift
@@ -0,0 +1,31 @@
+//
+//  FavoritePrimesView.swift
+//  ComposableArchitecture
+//
+//  Created by Milko Škofič on 19/10/2020.
+//
+
+import Combine
+import SwiftUI
+
+struct FavoritePrimesView: View {
+    @ObservedObject var store: Store<AppState, AppAction>
+
+    var body: some View {
+        List {
+            ForEach(self.store.value.favoritePrimes, id: \.self) { prime in
+                Text("\(prime)")
+            }
+            .onDelete { indexSet in
+                self.store.send(.favoritePrimes(.deleteFavoritePrimes(indexSet)))
+            }
+        }
+        .navigationBarTitle(Text("Favorite Primes"))
+    }
+}
+
+//struct FavoritePrimesView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FavoritePrimesView()
+//    }
+//}
--- ComposableArchitecture/Reducers.swift
+++ ComposableArchitecture/Reducers.swift
@@ -8,31 +8,117 @@
 import Combine
 import SwiftUI
 
+
 //
-// Application reducer.
+// Counter view reducer,
 //
-func appReducer(state: inout AppState, action: AppAction) {
+func counterReducer(state: inout Int, action: AppAction) {
     switch action {
     
     case .counter(.decrTapped):
-        state.count -= 1
+        state -= 1
         
     case .counter(.incrTapped):
-        state.count += 1
+        state += 1
         
+    default:
+        break
+    }
+}
+
+//
+// Prime modal view reducer,
+//
+func primeModalReducer(state: inout AppState, action: AppAction) {
+    switch action {
+    
     case .primeModal(.saveFavoritePrimeTapped):
-        state.favoritePrimes.append(state.count)
-        state.activityFeed.append(.init(timestamp: Date(), type: .addedFavoritePrime(state.count)))
-        
-    case .primeModal(.removeFavoritePrimeTapped):
-        state.favoritePrimes.removeAll(where: { $0 == state.count })
-        state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(state.count)))
-        
+         state.favoritePrimes.append(state.count)
+         state.activityFeed.append(.init(timestamp: Date(), type: .addedFavoritePrime(state.count)))
+         
+     case .primeModal(.removeFavoritePrimeTapped):
+         state.favoritePrimes.removeAll(where: { $0 == state.count })
+         state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(state.count)))
+
+   default:
+        break
+    }
+}
+
+//
+// Favourite primes view reducer,
+//
+func favoritePrimesReducer(state: inout FavoritePrimesState, action: AppAction) {
+    switch action {
+    
     case let .favoritePrimes(.deleteFavoritePrimes(indexSet)):
         for index in indexSet {
             let prime = state.favoritePrimes[index]
             state.favoritePrimes.remove(at: index)
             state.activityFeed.append(.init(timestamp: Date(), type: .removedFavoritePrime(prime)))
         }
+
+    default:
+        break
     }
 }
+
+//
+// Favourite primes state struct.
+//
+struct FavoritePrimesState {
+    var favoritePrimes: [Int]
+    var activityFeed: [AppState.Activity]
+}
+
+//
+// Extend AppState.
+//
+extension AppState {
+    var favoritePrimesState: FavoritePrimesState {
+        get {
+            FavoritePrimesState(
+                favoritePrimes: self.favoritePrimes,
+                activityFeed: self.activityFeed
+            )
+        }
+        set {
+            self.favoritePrimes = newValue.favoritePrimes
+            self.activityFeed   = newValue.activityFeed
+        }
+    }
+}
+
+//
+// Reducers combiner.
+//
+func combine<Value, Action>(
+    _ reducers: (inout Value, Action) -> Void...
+) -> (inout Value, Action) -> Void {
+    return { value, action in
+        for reducer in reducers {
+            reducer(&value, action)
+        }
+    }
+}
+
+//
+// Pullback function.
+//
+func pullback<LocalValue, GlobalValue, Action>(
+    _ reducer: @escaping (inout LocalValue, Action) -> Void,
+    value: WritableKeyPath<GlobalValue, LocalValue>
+) -> (inout GlobalValue, Action) -> Void {
+    return { globalValue, action in
+      reducer(&globalValue[keyPath: value], action)
+    }
+}
+
+//
+// Application reducer.
+//
+let appReducer = combine(
+    pullback(counterReducer, value: \.count),
+    primeModalReducer,
+    pullback(favoritePrimesReducer, value: \.favoritePrimesState)
+)
